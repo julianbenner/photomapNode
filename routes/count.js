9129588702caf59622b3eb156f83f1877df30c21
Julian Benner <julben23@gmail.com>
2015-01-31 20:48:36 +0100

    Initial commit

---
+++ routes/count.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+function get_image_count(lat_min, lat_max, lon_min, lon_max, callback) {
+    var query = 'SELECT image_count, slat/image_count as avg_lat, slon/image_count as avg_lon FROM ' +
+        '(SELECT COUNT(*) as image_count, SUM(lat) as slat, SUM(lon) as slon FROM ' +
+        'photomap_image WHERE lat BETWEEN '+connection.escape(lat_min)+' AND '+connection.escape(lat_max)+' AND lon BETWEEN '+connection.escape(lon_min)+' AND '+connection.escape(lon_max)+') t';
+    connection.query(
+        query, function(err, rows, fields) {
+        if (err) throw err;
+        callback(rows[0]);
+    });
+}
+
+/* GET users listing. */
+router.get('/:lat_min,:lat_max,:lon_min,:lon_max', function(req, res, next) {
+
+  //res.send(req.params.lat_min + req.params.lon_max);
+    get_image_count(req.params.lat_min, req.params.lat_max, req.params.lon_min, req.params.lon_max, function(data) { res.json(data); });
+});
+
+module.exports = router;
