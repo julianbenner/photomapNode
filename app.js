cd9fda628cd619b124ee99c7e103217d744e0390
Julian Benner <julianbenner@users.noreply.github.com>
2015-02-09 22:46:12 +0100

    Everything is broken

--- app.js
+++ app.js
@@ -1,35 +1,91 @@
 var express = require('express');
-var mysql      = require('mysql');
 
 var count = require('./routes/count');
 
+require('./routes/Database').Init();
+
 var app = express();
 
-connection = mysql.createConnection({
-    host     : '10.8.0.1',
-    port     : 33061,
-    user     : 'banane',
-    password : 'banane',
-    database : 'photomap'
-});
+app.set('views', './resources/views');
+app.set('view engine', 'jade');
+
 app.use(express.static(__dirname + '/public'));
 
-connection.connect();
+
+
+
+var passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy;
+
+var users = [
+  { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' },
+  { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
+
+function findByUsername(username, fn) {
+  console.log(10);
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // Find the user by username.  If there is no user with the given
+      // username, or the password is not correct, set the user to `false` to
+      // indicate failure and set a flash message.  Otherwise, return the
+      // authenticated `user`.
+      findByUsername(username, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+        return done(null, user);
+      });
+    });
+  }
+));
+
+app.use(require('connect-flash')());
+app.use(require('express-session')({secret:'hjdgft'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/get_image_count', count);
-
-app.use(function(req, res) {
-    res.send(404);
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+app.use('/admin', require('./routes/admin')());
 
 // error handlers
+
+// catch 404 and forward to error handlersno
+app.use(function(req, res) {
+    res.status(400);
+    res.render('error', {title: 'Error 404', error: req.url + ' does not exist on this server.'});
+});
 
 // development error handler
 // will print stacktrace
@@ -37,8 +93,8 @@ if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
-            message: err.message,
-            error: err
+            error: err,
+            message: err.stack
         });
     });
 }
@@ -46,10 +102,9 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+    res.status(err.status || 500);  
     res.render('error', {
-        message: err.message,
-        error: {}
+        error: err
     });
 });
 
--- public/javascripts/main.js
+++
@@ -1,1 +0,0 @@
-var script = require('./script');
