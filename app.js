e2ae2db79097106318fd95a941aff7e1993ec22d
Julian Benner <julben23@gmail.com>
2015-02-10 11:22:01 +0100

    Simple login

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 var count = require('./routes/count');
 
@@ -6,75 +8,17 @@ require('./routes/Database').Init();
 
 var app = express();
 
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.set('views', './resources/views');
 app.set('view engine', 'jade');
 
+
 app.use(express.static(__dirname + '/public'));
-
-
-
-
-var passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy;
-
-var users = [
-  { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' },
-  { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
-];
-
-function findById(id, fn) {
-  var idx = id - 1;
-  if (users[idx]) {
-    fn(null, users[idx]);
-  } else {
-    fn(new Error('User ' + id + ' does not exist'));
-  }
-}
-
-function findByUsername(username, fn) {
-  console.log(10);
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-    if (user.username === username) {
-      return fn(null, user);
-    }
-  }
-  return fn(null, null);
-}
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  findById(id, function (err, user) {
-    done(err, user);
-  });
-});
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      
-      // Find the user by username.  If there is no user with the given
-      // username, or the password is not correct, set the user to `false` to
-      // indicate failure and set a flash message.  Otherwise, return the
-      // authenticated `user`.
-      findByUsername(username, function(err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
-        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
-        return done(null, user);
-      });
-    });
-  }
-));
-
-app.use(require('connect-flash')());
-app.use(require('express-session')({secret:'hjdgft'}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/get_image_count', count);
 app.use('/admin', require('./routes/admin')());
@@ -83,29 +27,32 @@ app.use('/admin', require('./routes/admin')());
 
 // catch 404 and forward to error handlersno
 app.use(function(req, res) {
-    res.status(400);
-    res.render('error', {title: 'Error 404', error: req.url + ' does not exist on this server.'});
+  res.status(400);
+  res.render('error', {
+    title: 'Error 404',
+    error: req.url + ' does not exist on this server.'
+  });
 });
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            error: err,
-            message: err.stack
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      error: err,
+      message: err.stack
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);  
-    res.render('error', {
-        error: err
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    error: err
+  });
 });
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,9 @@
 var express = require('express');
-
+var passport = require('passport');
+var flash = require('connect-flash');
+var session = require('express-session');
+var path = require('path');
+var LocalStrategy = require('passport-local').Strategy;
 
 function get_image_count(lat_min, lat_max, lon_min, lon_max, callback) {
   var query = 'SELECT image_count, slat/image_count as avg_lat, slon/image_count as avg_lon FROM ' +
@@ -12,23 +16,119 @@ function get_image_count(lat_min, lat_max, lon_min, lon_max, callback) {
       callback(rows[0]);
     });
 }
+
+var users = [{
+  id: 1,
+  username: 'bob',
+  password: 'secret',
+  email: 'bob@example.com'
+}, {
+  id: 2,
+  username: 'joe',
+  password: 'birthday',
+  email: 'joe@example.com'
+}];
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
+
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function() {
+
+      // Find the user by username.  If there is no user with the given
+      // username, or the password is not correct, set the user to `false` to
+      // indicate failure and set a flash message.  Otherwise, return the
+      // authenticated `user`.
+      findByUsername(username, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, {
+            message: 'Unknown user ' + username
+          });
+        }
+        if (user.password != password) {
+          return done(null, false, {
+            message: 'Invalid password'
+          });
+        }
+        return done(null, user);
+      })
+    });
+  }
+));
+
 module.exports = function admin() {
   var router = express.Router();
 
-var passport = require('passport');
+  var relPath = '/admin';
 
-  router.post('/login', passport.authenticate('local', {
-    successRedirect: '/admin',
-    failureRedirect: '/admin/login',
-    failureFlash: true
-  }));
+  router.use(session({
+    secret: 'ilovescotchscotchyscotchscotch'
+  })); // session secret
+  router.use(passport.initialize());
+  router.use(passport.session()); // persistent login sessions
+  router.use(flash());
+
+  router.post('/login',
+    passport.authenticate('local', {
+      failureRedirect: 'login',
+      failureFlash: true
+    }),
+
+    function(req, res) {
+      res.redirect(path.join(relPath, '/'));
+    });
 
   router.get('/', function(req, res) {
-    res.render('admin');
+    res.render('admin', {
+      user: req.user
+    });
   });
 
   router.get('/login', function(req, res) {
-    res.render('login');
+    if (req.user) {
+      res.redirect(path.join(relPath, '/'));
+    } else {
+      res.render('login', {
+        message: req.flash('error'),
+        user: req.user
+      });
+    }
+  });
+
+  router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect(path.join(relPath, '/'));
   });
 
   return router;
