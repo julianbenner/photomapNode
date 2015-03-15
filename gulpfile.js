bb6d62d793e004cfea81626e0969bd83638d4733
Julian Benner <julianbenner@users.noreply.github.com>
2015-03-15 16:38:13 +0100

    Cleanup, updates, fixes

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,12 @@
 var gulp = require('gulp'),
 	sass = require('gulp-ruby-sass'),
 	autoprefix = require('gulp-autoprefixer'),
-	notify = require("gulp-notify"),
-	bower = require('gulp-bower');
+	notify = require("gulp-notify");
 
 var config = {
 	sassPath: './resources/sass',
 	bowerDir: './bower_components'
 };
-
-gulp.task('bower', function() {
-	return bower()
-		.pipe(gulp.dest('./public/javascripts'));
-});
-
-gulp.task('icons', function() {
-	return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
-		.pipe(gulp.dest('./public/fonts'));
-});
 
 gulp.task('css', function() {
 	return sass(config.sassPath + '/style.scss', {
@@ -42,4 +31,4 @@ gulp.task('watch', function() {
 	gulp.watch(config.sassPath + '/**/*.scss', ['css']);
 });
 
-gulp.task('default', ['bower', 'icons', 'css']);
+gulp.task('default', ['css']);
--- routes/count.js
+++ routes/count.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var is_production = function() {
+    try {
+        return express().get('env') === 'production';
+    } catch (e) {
+        return false;
+    }
+};
+
 function get_image_count(lat_min, lat_max, lon_min, lon_max, dateMin, dateMax, callback) {
     var connection = require('../routes/Database').Get();
     
@@ -16,11 +24,19 @@ function get_image_count(lat_min, lat_max, lon_min, lon_max, dateMin, dateMax, c
     '(SELECT COUNT(*) as image_count, SUM(lat) as slat, SUM(lon) as slon FROM ' +
     'photomap_image WHERE ' + constraints + ') t';
 
+    if (!is_production())
     console.log(query);
 
     connection.query(query, function(err, rows, fields) {
-        if (err) throw err;
-        callback(rows[0]);
+        if (err) {
+            if (is_production())
+                callback({"SUCCESS": false});
+            else
+                callback({"SUCCESS": false, "ERROR": err.code});
+        } else {
+            rows[0].SUCCESS = true;
+            callback(rows[0]);
+        }
     });
 }
 
